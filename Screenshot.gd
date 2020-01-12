0fa8708b2877213f61e040abd902f131a69ecf11
Filip Lundby <filip@filiplundby.dk>
2020-01-12 11:21:27 +0100

    Added random number to avoid overwriting existing files

--- Screenshot.gd
+++ Screenshot.gd
@@ -1,12 +1,15 @@
 extends Node
 
 # Screenshots without hiccups using threads.
-# Attach the script to a Node and hit F10
+# Attach the script to a Node, start your game and hit F11
 # 
-# Tested in Godot 3.2 beta 4, Linux
+# Tested in Godot 3.2 beta 5, Linux
+# By Filip Lundby, https://twitter.com/skooterkurt
 
-var _screenshot_directory = "user://screenshots"
+var _root_directory = "user://"
+var _screenshot_directory = "screenshots"
 var _capture_tasks = []
+var _random = RandomNumberGenerator.new()
 
 func _ready():
 	# Create directory
@@ -14,7 +17,7 @@ func _ready():
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
-		if event.scancode == KEY_F10:
+		if event.scancode == KEY_F11:
 			_capture()
 
 func _capture():
@@ -25,11 +28,18 @@ func _capture():
 
 func _capture_thread(_arg):
 	# Save image
+	_random.randomize()
+	var image_filename = "capture_%s%s.png" % [OS.get_unix_time(), _random.randi_range(100000, 999999)]
+	var godot_path = "%s/%s/%s" % [_root_directory, _screenshot_directory, image_filename]
 	var image = get_viewport().get_texture().get_data()
-	var path = "%s/capture_%s.png" % [_screenshot_directory, OS.get_unix_time()]
 	image.flip_y()
-	image.save_png(path)
-	print ("Screenshot saved to: %s%s" % [OS.get_user_data_dir(), path])
+	image.save_png(godot_path)
+	# Print path to image
+	var os_path = [OS.get_user_data_dir(), _screenshot_directory, image_filename]
+	if OS.get_name() == "Windows":
+		print ("Screenshot saved to: %s\\%s\\%s" % os_path)
+	else:
+		print ("Screenshot saved to: %s/%s/%s" % os_path)
 
 func _exit_tree():
 	for task in _capture_tasks:
