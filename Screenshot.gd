e47a7cd6c73227d229f833a66e62cc3572493399
Filip Lundby <filip@filiplundby.dk>
2020-01-13 18:00:51 +0100

    Removing OS check when build path

--- Screenshot.gd
+++ Screenshot.gd
@@ -10,12 +10,10 @@ var _root_directory = "user://"
 var _screenshot_directory = "screenshots"
 var _capture_tasks = []
 var _random = RandomNumberGenerator.new()
-var _os_separator = "/"
 
 func _ready():
 	# Create directory
-	_os_separator = "\\" if OS.get_name() == "Windows" else "/"
-	var screenshot_directory = PoolStringArray([_root_directory, _screenshot_directory]).join(_os_separator)
+	var screenshot_directory = "%s/%s" % [_root_directory, _screenshot_directory]
 	Directory.new().make_dir(screenshot_directory)
 
 func _input(event):
@@ -37,8 +35,10 @@ func _capture_thread(_arg):
 	var image = get_viewport().get_texture().get_data()
 	image.flip_y()
 	image.save_png(godot_path)
-	# Print path to image
-	var os_path = PoolStringArray([OS.get_user_data_dir(), _screenshot_directory, image_filename]).join(_os_separator)
+	
+	# Print path to image (just for debugging - remove if you like)
+	var os_separator = "\\" if OS.get_name() == "Windows" else "/"
+	var os_path = PoolStringArray([OS.get_user_data_dir(), _screenshot_directory, image_filename]).join(os_separator)
 	print ("Screenshot saved to: %s" % os_path)
 
 func _exit_tree():
